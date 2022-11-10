31e578c1a8472c2afdc7470369f9707a62ed2a3a
John Lazaroo <jlazaroo@gmail.com>
2022-11-10 11:06:43 -0800

    Add Task button creates tasks and renders cards

--- js/index.js
+++ js/index.js
@@ -1,36 +1,79 @@
 const taskName = document.querySelector('#taskName');
 const dueDate = document.querySelector('#dueDate');
 const taskDescription = document.querySelector('#taskDescription');
+const descriptionCount = document.querySelector('#descriptionCount')
 const assignee = document.querySelector('#assignee');
 const submit = document.querySelector('#submit');
 const submitAlert = document.querySelector('#submitAlert');
+const taskList = document.querySelector('#taskList');
+
+// Keeps track of characters typed and displays the current amount
+let taskDescriptionCount = 0;
+taskDescription.addEventListener("input", function(){
+    descriptionCount.textContent = taskDescription.value.length;
+});
+
+// Makes new date object
+let currentDate = new Date();
+// Gets current year, month and day
+let year = currentDate.getFullYear();
+let month = currentDate.getMonth() + 1;
+let day = currentDate.getDate();
+// Prepends numbers 0-9 with a leading zero so they are two digits
+if(day < 10 ){
+    day = '0' + day.toString();
+}
+// Sets a 'minimum' date so an earlier date (than today) cannot be picked
+dueDate.setAttribute('min', `${year}-${month}-${day}`);
+
+// creates new task array
+const taskArray = new TaskManager();
 
 // Assigns the mouse click on the submit button to the submitClick Function
 submit.onclick = submitClick;
-
 // Function is triggered by the submit button mouse click
 function submitClick(event) {
-    // this prevents the page reloading because of the form submission
-
     // checks if any field is not filled or checked and toggles the 'hide' class for the alert
     if(assignee.selectedIndex < 1 || taskName.value == '' || dueDate.value == '' || taskDescription.value == ''){
+        // event.preventDefault();
         // only toggles if the 'hide' class is there
         if(submitAlert.classList.contains('hide')){
             submitAlert.classList.toggle('hide');
         }
     //only toggles if the 'hide' class is not there
-    } else if(submitAlert.classList.contains('hide') == false){
-        submitAlert.classList.toggle('hide');
+    } else {
         event.preventDefault();
+        // only toggles if the 'hide' class is NOT there
+        if(submitAlert.classList.contains('hide') == false){
+            submitAlert.classList.toggle('hide');
+        }
+        // Calls addTask() and uses values from the form
+        taskArray.addTask(taskName.value, taskDescription.value, assignee.options[assignee.selectedIndex].text, dueDate.value);
+        taskArray.render();
+        // Initializes all inputs in form
+        assignee.selectedIndex = 0;
+        taskName.value = '';
+        dueDate.value = '';
+        taskDescription.value = '';
+        descriptionCount.textContent = 0;
     }
 }
 
-// creates new task array
-const taskArray = new TaskManager();
-// adds new tasks to task array just created
+
+// adds new tasks for test purposes
 taskArray.addTask('Task 1', 'Description of task 1', 'John', '2022-11-10');
 taskArray.addTask('Task 2', 'Description of task 2', 'Megan', '2022-11-12');
 taskArray.addTask('Task 3', 'Description of task 3', 'Corbin', '2022-11-14');
 taskArray.addTask('Task 4', 'Description of task 4', 'Zinash', '2022-11-13');
 taskArray.addTask('Task 5', 'Description of task 5', 'Wenbo', '2022-11-15');
+taskArray.render();
+
+
+taskList.addEventListener('click', (event) => { // "event" here is the event parameter
+    if(event.target.classList.contains('doneButton')){
+        // console.log(event.target.parentElement.parentElement.parentElement);
+        console.log(event.target)
+    }
+    
+});
 
--- js/taskManager.js
+++ js/taskManager.js
@@ -1,15 +1,20 @@
 console.log('Group Project Task 5')
 
-const createTaskHtml = (name, description,assignedTo,dueDate,status) => {
+const createTaskHtml = (id, name, description,assignedTo,dueDate,status) => {
     const html = ` 
-        <li class="list-group-item" style="width: 18rem;">
+        <li class="list-group-item col-sm-6 col-lg-4" id="${id}">
+            <div class="row status-row">
+                <div class="col"></div>
+                <div class="col-3 btn btn-danger status">${status}</div>
+            </div>
             <div class="card bg-light">
                 <h5 class="card-header">${name}</h5>
                 <div class="card-body">
-                    <p class="card-text">${description}</p>
-                    <p class="card-text">${assignedTo}</p>
-                    <p class="card-text">${dueDate}</p>
-                    <a href="" class="btn btn-danger">${status}</a>
+                    <p class="card-text">Due: ${dueDate}</p>
+                    <p class="card-text">Assigned to: ${assignedTo}</p>
+                    <p class="card-text card-description">${description}</p>
+                    <a class="btn btn-success rounded-pill doneButton">Done</a>
+                    <a class="btn btn-danger rounded-pill">Delete</a>
                 </div>
             </div>
         </li>
@@ -38,13 +43,13 @@ class TaskManager {
         this.tasks.forEach(task => {
             const date = new Date(task.dueDate)
             const formattedDate = date.toDateString()
-            const taskHtml = createTaskHtml(task.name, task.description, task.assignedTo, formattedDate, task.status)
+            const taskHtml = createTaskHtml(task.id, task.name, task.description, task.assignedTo, formattedDate, task.status)
             tasksHtmlList.push(taskHtml)
         }) 
         const tasksHtml = tasksHtmlList.join('\n')
-        const taskList = document.querySelector("#task-list")
+        const taskList = document.querySelector("#taskList")
         taskList.innerHTML = tasksHtml
-        console.log(taskList)
-        console.log(tasksHtml)
+        // console.log(taskList)
+        // console.log(tasksHtml)
     }
 }
